b3aab29755072530c7b4aa86be7429a08cdfc619
Pier Bover <pierbover11@gmail.com>
2021-12-24 12:06:26 -0600

    global default settings

--- demo-app/src/main.js
+++ demo-app/src/main.js
@@ -1,6 +1,13 @@
 import {initRouter} from 'pluma-svelte-router';
+import FormController from '../../src/FormController.js';
 import App from './components/App.svelte';
 import routerConfig from './router.js';
+
+// This is how you could set global default settings
+// FormController.setDefaultSettings({
+// 	validClass: 'is-valid',
+// 	invalidClass: 'is-invalid'
+// });
 
 initRouter(routerConfig);
 
--- src/FormController.js
+++ src/FormController.js
@@ -7,84 +7,97 @@ const {VALID, INVALID, PENDING} = ValidationStates;
 const {INSTANT_AFTER_SUBMIT} = ValidateOn;
 
 export default class FormController {
-	constructor (config) {
 
+	static defaultSettings = {
+		useNativeErrorTooltips: false,
+		validClass: 'valid',
+		invalidClass: 'invalid',
+		validateOn: INSTANT_AFTER_SUBMIT,
+		addValidClassToAllInputs: false,
+		removeValidationClassesOnSubmit: true
+	};
+
+	static setDefaultSettings (settings) {
+		for (const [key, value] of Object.entries(settings)) {
+			if (this.defaultSettings.hasOwnProperty(key)) this.defaultSettings[key] = value;
+		}
+	}
+
+	constructor (config) {
+		
 		this.stores = {};
 		this.fields = [];
-
+		
 		this.formSubmitState = FormStates.UNSUBMITTED;
 		this.formValidationState = PENDING;
-
+		
 		// Bind handlers
 		this.onSubmitEvent = this.onSubmitEvent.bind(this);
-
+		
 		// Settings
 		this.settings = {};
-		this.settings.errorMessages = config.errorMessages || {};
 		this.settings.onSubmitCallback = config.onSubmit;
-		this.settings.useNativeErrorTooltips = getValueOrDefault(config.useNativeErrorTooltips, false);
-		this.settings.validClass = getValueOrDefault(config.validClass, 'valid');
-		this.settings.invalidClass = getValueOrDefault(config.invalidClass, 'invalid');
-		this.settings.validateOn = getValueOrDefault(config.validateOn, INSTANT_AFTER_SUBMIT);
-		this.settings.clearErrorsOnInput = getValueOrDefault(config.clearErrorsOnInput, true);
-		this.settings.hideErrorsOnChange = getValueOrDefault(config.hideErrorsOnChange, false);
-		this.settings.addValidClassToAllInputs = getValueOrDefault(config.addValidClassToAllInputs, false);
-		this.settings.instantValidationTimeout = getValueOrDefault(config.instantValidationTimeout, 250);
-		this.settings.removeValidationClassesOnSubmit = getValueOrDefault(config.removeValidationClassesOnSubmit, true);
-
+		this.settings.useNativeErrorTooltips = getValueOrDefault(config.useNativeErrorTooltips, FormController.defaultSettings.useNativeErrorTooltips);
+		this.settings.validClass = getValueOrDefault(config.validClass, FormController.defaultSettings.validClass);
+		this.settings.invalidClass = getValueOrDefault(config.invalidClass, FormController.defaultSettings.invalidClass);
+		this.settings.validateOn = getValueOrDefault(config.validateOn, FormController.defaultSettings.validateOn);
+		// Setting to force valid class on checkboxes, radio buttons, and selects
+		this.settings.addValidClassToAllInputs = getValueOrDefault(config.addValidClassToAllInputs, FormController.defaultSettings.addValidClassToAllInputs);
+		this.settings.removeValidationClassesOnSubmit = getValueOrDefault(config.removeValidationClassesOnSubmit, FormController.defaultSettings.removeValidationClassesOnSubmit);
+		
 		this.initFieldsSettings(config.fields || {});
 		this.initFields();
-
+		
 		// Init form
 		this.formElement = config.form;
-
+		
 		// Disable native error tooltips
 		if (!this.settings.useNativeErrorTooltips) this.formElement.setAttribute('novalidate', true);
-
+		
 		this.formElement.addEventListener('submit', this.onSubmitEvent);
-
+		
 		// DOM Mutation Observer
 		this.observer = new MutationObserver(this.onDOMMutation.bind(this));
 		this.observer.observe(this.formElement, {attributes: false, childList: true, subtree: true});
-
+		
 		this.updateFieldsFromDom();
 		this.stores.formState = config.formState;
 		this.stores.errors = config.errors;
 		this.updateFormState();
 	}
-
+	
 	destroy () {}
-
+	
 	onSubmitEvent (event) {
-
+		
 		event.preventDefault();
-
+		
 		this.validateAllFields();
 		this.updateFormState();
-
+		
 		if (this.formValidationState === VALID) {
 			if (this.settings.removeValidationClassesOnSubmit) this.removeCssClassesFromFields();
 			this.formSubmitState = FormStates.SUBMITTED;
 			this.settings.onSubmitCallback(this.getFormValues());
 		}
 	}
-
+	
 	onDOMMutation (event) {
 		window.requestAnimationFrame(() => {
 			this.updateFieldsFromDom();
 			this.updateFormState();
-		})
+		});
 	}
-
+	
 	// STATE
-
+	
 	getFormState () {
 		const state = {
 			state: this.formSubmitState,
 			validationState: this.formValidationState,
 			touched: false
 		}
-
+		
 		this.fields
 		// Get the group fields value last
 		.sort((field) => {
@@ -96,26 +109,26 @@ export default class FormController {
 				const fieldState = field.getState();
 				state[field.name] = fieldState;
 			}
-
+			
 			if (field.touched) state.touched = true;
 		});
-
+		
 		return state;
 	}
-
+	
 	getDisplayedErrors () {
 		const errors = {};
-
+		
 		this.fields.forEach((field) => {
 			if (!field.isOrphan && field.displayError && field.error) errors[field.name] = field.error;
 		});
-
+		
 		return errors;
 	}
-
+	
 	updateFormValidationState () {
 		let newValidationState = VALID;
-
+		
 		this.fields.forEach((field) => {
 			// Ignore orphan fields
 			if (field.isOrphan) return;
@@ -124,90 +137,90 @@ export default class FormController {
 				if (field.validationState !== VALID) newValidationState = field.validationState;
 			}
 		});
-
+		
 		this.formValidationState = newValidationState;
 	}
-
+	
 	updateFormState () {
 		this.updateFormValidationState();
 		this.updateStores();
 	}
-
+	
 	updateStores () {
 		if (this.stores.formState) {
 			const formState = this.getFormState();
 			this.stores.formState.set(formState);
 		}
-
+		
 		if (this.stores.errors) this.stores.errors.set(this.getFormErrors());
 	}
-
+	
 	getFormErrors (formState) {
 		const errors = {};
-
+		
 		this.fields.forEach((field) => {
 			if (field.error) errors[field.name] = field.error;
 		});
-
+		
 		return errors;
 	}
-
+	
 	// FIELDS
-
+	
 	initFieldsSettings (fieldsSettings) {
 		this.fieldsSettings = Object.keys(fieldsSettings).map((name) => {
-
+			
 			const settings = fieldsSettings[name];
-
+			
 			settings.name = name;
 			settings.type = FieldTypes.HTML;
 			settings.controller = this;
-
+			
 			if (!settings.validateOn) settings.validateOn = this.settings.validateOn;
-
+			
 			if (settings.fields) {
 				settings.type = FieldTypes.GROUP;
 			}
-
+			
 			return settings;
 		});
 	}
-
+	
 	// Init the fields in the settings config
-
+	
 	initFields() {
 		const groups = [];
-
+		
 		this.fieldsSettings.forEach((settings) => {
-
+			
 			switch (settings.type) {
 				case FieldTypes.HTML:
-					settings.parentGroupField = this.getParentGroupField(settings.name);
-					this.fields.push(new HtmlField(settings));
-					break;
+				settings.parentGroupField = this.getParentGroupField(settings.name);
+				this.fields.push(new HtmlField(settings));
+				break;
 				case FieldTypes.GROUP:
-					this.fields.push(new GroupField(settings));
-					break;
+				this.fields.push(new GroupField(settings));
+				break;
 			}
 		});
 	}
-
+	
 	getFieldByName (name) {
 		return this.fields.find((field) => field.name === name);
 	}
-
+	
 	// Create or update fields for each form element
 	updateFieldsFromDom () {
 		const inputElements = getFormInputElements(this.formElement);
-
+		
 		const radioGroups = {};
-
+		
 		// Get all radio groups
-
+		
 		inputElements.forEach((element) => {
 			if (element.type === InputTypes.RADIO) {
 				const name = element.name;
-
+				
 				if (!radioGroups[name]) {
 					radioGroups[name] = [element];
 				} else {
@@ -215,11 +228,11 @@ export default class FormController {
 				}
 			}
 		});
-
+		
 		inputElements.forEach((element) => {
-
+			
 			let elements;
-
+			
 			if (element.type === InputTypes.RADIO) {
 				let group = radioGroups[element.name];
 				if (!group) return;
@@ -228,16 +241,16 @@ export default class FormController {
 			} else {
 				elements = [element];
 			}
-
+			
 			const field = this.getFieldByName(elements[0].name);
-
+			
 			if (field && field.type === FieldTypes.HTML) {
 				// If the field already exists, just update the element
 				field.updateElements(elements);
 			} else {
 				// If not, create a new field and use config settings (if any)
 				let settings = this.fieldsSettings.find((settings) => settings.name === element.name);
-
+				
 				// If there are no settings for this field, create blank settings
 				if (!settings) {
 					settings = {
@@ -248,21 +261,21 @@ export default class FormController {
 						parentGroupField: this.getParentGroupField(element.name)
 					};
 				}
-
+				
 				settings.elements = elements;
-
+				
 				this.fields.push(new HtmlField(settings));
 			}
 		});
 	}
-
+	
 	getParentGroupField (childName) {
 		for (let i = 0; i < this.fields.length; i++) {
 			const field = this.fields[i];
 			if (field.type === FieldTypes.GROUP && field.fields.indexOf(childName) !== -1) return field;
 		}
 	}
-
+	
 	validateAllFields () {
 		this.fields
 		// Validate the group fields value last
@@ -274,20 +287,20 @@ export default class FormController {
 			field.onEvent({type: FormEvents.SUBMIT});
 		});
 	}
-
+	
 	removeCssClassesFromFields () {
 		this.fields.forEach((field) => {
 			field.setCssClass(null);
 		});
 	}
-
+	
 	getFormValues () {
 		const values = {};
-
+		
 		this.fields.forEach((field) => {
 			values[field.name] = field.value
 		});
-
+		
 		return values;
 	}
 }
